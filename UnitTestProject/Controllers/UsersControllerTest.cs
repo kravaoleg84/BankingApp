ab3d463d842ed0d6da07a7568f8bee97757ee012
kravaoleg84 <kravaoleg84@ukr.net>
2017-06-25 20:05:14 +0300

    add test methods for UsersController

--- UnitTestProject/Controllers/UsersControllerTest.cs
+++ UnitTestProject/Controllers/UsersControllerTest.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankingApp.WEB.Controllers;
+using BankingApp.BLL.Interfaces;
+using Moq;
+using BankingApp.BLL.ViewModels;
 
 namespace BankingApp.Tests.Controllers
 {
@@ -7,9 +11,31 @@ namespace BankingApp.Tests.Controllers
     public class UsersControllerTest
     {
         [TestMethod]
-        public void GetActionResultNotNull()
+        public void GetReturnsUserDTOWithSameId()
         {
-            UsersController controller = new UsersController();
+            int id = 1;
+            var mock = new Mock<IUserService>();
+            mock.Setup(a => a.GetUser(id)).Returns(new UserDTO { Id = 1 });
+            var controller = new UsersController(mock.Object);
+
+            IHttpActionResult actionResult = controller.Get(id);
+            var contentResult = actionResult as OkNegotiatedContentResult<UserDTO>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(1, contentResult.Content.Id);
+        }
+
+        [TestMethod]
+        public void GetReturnsNotFound()
+        {
+            int id = 1;
+            var mock = new Mock<IUserService>();
+            var controller = new UsersController(mock.Object);
+
+            IHttpActionResult actionResult = controller.Get(id);
+
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
         }
     }
 }
